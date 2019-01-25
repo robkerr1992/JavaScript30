7abf1b28a6511c1946f8e9a96192fdd1475d4fd7
Robert Kerr <rob@robertkerr.me>
2019-01-24 21:44:37 -0600

    figured out the bugs, still figuring out fullscreen

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,8 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('player__slider');  
+const ranges = player.querySelectorAll('.player__slider');  
+const fullscreen = player.querySelector('.player__fullscreen');  
 
 //functions
 function togglePlay(){
@@ -29,7 +30,14 @@ function handleProgress(){
 }
 
 function handleScrubbing(e) {
-    video.currentTime = (e.offsetX/progressBar.offsetWidth) * video.duration;
+    video.currentTime = (e.offsetX/progress.offsetWidth) * video.duration;
+}
+
+function handleScreenSizing() {
+    console.dir(video);
+    console.dir(document);
+    // video.he
+    player.classList.toggle('fullscreen');
 }
 
 //event listeners
@@ -40,7 +48,7 @@ video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+fullscreen.addEventListener('click', handleScreenSizing);
 
 let mouseDown = false;
 progress.addEventListener('click', handleScrubbing);
